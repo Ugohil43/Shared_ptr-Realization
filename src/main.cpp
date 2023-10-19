8b5cb6e2d5d0570586d8df6dcab70af8dc48b768
EzhovI <yehov.ru@mail.ru>
2023-10-19 15:42:45 +0300

    A class has been written that implements the operation of shared_ptr.

--- src/main.cpp
+++ src/main.cpp
@@ -2,43 +2,77 @@
 #include <memory>
 #include <string>
 
-#include "MyClass.hpp"
+#include "My_shared_ptr.hpp"
 
 //cmake -G "MinGW Makefiles" ../
 
+void check_ptr()
+{
+    My_shared_ptr<int> p1(new int(100));
+    My_shared_ptr<int> p2;
+    My_shared_ptr<int> p3(std::move(p1));
+    My_shared_ptr<int> p4(new int(200));
+
+    std::cout << "Use_count p1: " << p1.use_count() << std::endl;
+    std::cout << "p2 is " << p2.operator bool() << std::endl;
+    std::cout << "Use_count p3: " << p3.use_count() << std::endl;
+    std::cout << "Use_count p4: " << p4.use_count() << std::endl << std::endl;
+    
+    std::cout << "Data p1: " << *p1 << std::endl;    
+    std::cout << "Data p3: " << *p3 << std::endl;    
+    std::cout << "Data p4: " << *p4 << std::endl << std::endl << std::endl;    
+    
+    std::cout << "test_1 p2 = p1" << std::endl;
+    p2 = p1;
+    std::cout << "Use_count p2: " << p2.use_count() << std::endl;
+    std::cout << "Data p2: " << *p2 << std::endl;
+    std::cout << "Use_count p1: " << p1.use_count() << std::endl;
+    std::cout << "Data p1: " << *p1 << std::endl << std::endl << std::endl;
+
+
+    std::cout << "test_2 p2 = p3" << std::endl;
+    p2 = p3;
+    std::cout << "Use_count p2: " << p2.use_count() << std::endl;
+    std::cout << "Data p2: " << *p2 << std::endl;
+    std::cout << "Use_count p3: " << p3.use_count() << std::endl;
+    std::cout << "Data p3: " << *p3 << std::endl << std::endl << std::endl;
+
+    std::cout << "test_3 p1.swap(p4)" << std::endl;
+    p1.swap(p4);
+    std::cout << "Use_count p1: " << p1.use_count() << std::endl;
+    std::cout << "Data p1: " << *p1 << std::endl;
+    std::cout << "Use_count p4: " << p4.use_count() << std::endl;
+    std::cout << "Data p4: " << *p4 << std::endl << std::endl << std::endl;
+
+    std::cout << "test_4 p1 = move(p4)" << std::endl;
+    p1 = std::move(p4);
+    std::cout << "Use_count p1: " << p1.use_count() << std::endl;
+    std::cout << "Data p1: " << *p1 << std::endl;
+    std::cout << "p4 is " << p4.operator bool() << std::endl << std::endl;
+    //std::cout << "Use_count p4: " << p4.use_count() << std::endl;
+    //std::cout << "Data p4: " << *p4 << std::endl << std::endl;
+    //My_shared_ptr<int> p3(p4);
+    //std::cout << "Use_count p3: " << p3.use_count() << std::endl;
+    //std::cout << "Data p3: " << *p3 << std::endl << std::endl;
+    std::shared_ptr<int> sharedPtr1(new int(100));
+    std::shared_ptr<int> sharedPtr2(new int(200));
+    std::cout << "Data sharedPtr1: " << *sharedPtr1 << std::endl;
+    std::cout << "Data sharedPtr2: " << *sharedPtr2 << std::endl;
+    std::cout << "Use_count sharedPtr1: " << sharedPtr1.use_count() << std::endl;
+    std::cout << "Use_count sharedPtr2: " << sharedPtr2.use_count() << std::endl << std::endl;
+
+    std::cout << "test sharedPtr1 = std::move(sharedPtr2)" << std::endl;
+    sharedPtr1 = std::move(sharedPtr2);
+    std::cout << "Data sharedPtr1: " << *sharedPtr1 << std::endl;
+    std::cout << "Use_count sharedPtr1: " << sharedPtr1.use_count() << std::endl;
+    std::cout << "sharedPtr2.bool() " << sharedPtr2.operator bool() << std::endl;
+    //std::cout << "Data sharedPtr2: " << *sharedPtr2 << std::endl; 
+    //std::cout << "Use_count sharedPtr2: " << sharedPtr2.use_count() << std::endl << std::endl;
+}
+
 int main()
 {
-    std::cout << "Start of the program..." << std::endl<<std::endl;
-    
-    MyClass<int> value(123);
-    std::shared_ptr<MyClass<int>> sharedPtr1 = std::make_shared<MyClass<int>>(value);
-
-    // Создаем еще один shared_ptr, который также управляет объектом
-    std::shared_ptr<MyClass<int>> sharedPtr2 = sharedPtr1;
-
-    // Используем shared_ptr для вызова методов объекта
-    std::cout << "\nUsing printData() for SharedPtr1... " << std::endl;
-    sharedPtr1->printData();
-    std::cout << "Using printData() for SharedPtr2... " << std::endl;
-    sharedPtr2->printData();
-    std::cout << std::endl;
-
-    // Используем rvalue ссылки и std::move для передачи владения объектом
-    std::string value2 = "Value 2";
-    std::shared_ptr<MyClass<std::string>> sharedPtr3 = std::make_shared<MyClass<std::string>>(std::move(value2));
-    std::shared_ptr<MyClass<std::string>> sharedPtr4 = std::move(sharedPtr3);
-
-    // Попытка использования sharedPtr3 приведет к ошибке, так как теперь объектом владеет sharedPtr4
-    //sharedPtr3->printData(); // Ошибка: у sharedPtr3 нет владения объектом
-
-    std::cout << "\nUsing printData() for SharedPtr4... " << std::endl;
-    sharedPtr4->printData();
-
-    // Используем lvalue ссылки для возврата объекта из shared_ptr
-    std::shared_ptr<MyClass<std::string>> sharedPtr5 = sharedPtr4; // Создаем lvalue объект, который получает владение
-    std::cout << "\nUsing printData() for SharedPtr5... " << std::endl;
-    sharedPtr5->printData();
-
-    // Объект будет удален только после уничтожения последнего shared_ptr, управляющего им
+    std::cout << "Start program..." << std::endl << std::endl;
+    check_ptr();
     return 0;
 }
